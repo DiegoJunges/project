d0c3c6d4a4c3167c9a12ab7a2f59292cd0973441
Diego <diego.aviz@going2.com.br>
2021-07-06 22:04:59 -0300

    feat: initial setup

---
+++ src/modules/cars/useCases/createSpecification/CreateSpecificationUseCase.ts
@@ -0,0 +1,24 @@
+import { ISpecificationRepository } from "../../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
+class CreateSpecificationUseCase {
+  constructor(private specificatioRepository: ISpecificationRepository) {}
+  execute({ description, name }: IRequest): void {
+    const specificationAlreadyExists = this.specificatioRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists!");
+    }
+
+    this.specificatioRepository.create({
+      name,
+      description,
+    });
+  }
+}
+
+export { CreateSpecificationUseCase };
